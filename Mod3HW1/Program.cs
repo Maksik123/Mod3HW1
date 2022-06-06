3e2539c4261691bcc9c243cee4ac73b31841b3c7
Borysenko Maxim <borysenkoomax@gmail.com>
2022-06-06 12:31:44 +0300

    Problems w Sort and Increasing of array

--- Mod3HW1/Program.cs
+++ Mod3HW1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace Mod3HW1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var MyList = new MyList<int>(8);
+            var array = new int[] { 4, 3, 2, 1 , 0};
+            MyList.AddRange(array);
+            MyList.Add(5);
+            MyList.RemoveAt(0);
+            MyList.Remove(4);
+            MyList.Sort(new IntComparer());
+
+            foreach(var i in MyList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
